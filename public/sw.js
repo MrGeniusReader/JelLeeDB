725bdd7a2cb66cd88ec1d00988258dc0a27cbb29
MrGeniusReader <mrgeniusreader@gmail.com>
2026-02-03 04:48:15 +0000

    aksk

---
+++ public/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'jelleedb-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/images/jelleedb-icon.png'
+];
+
+// Install the service worker and cache assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Serve cached content when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
